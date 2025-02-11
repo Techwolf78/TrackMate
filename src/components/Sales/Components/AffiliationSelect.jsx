fd85df78ad9a2c7f5e33f72dfb2ff954cb19ebff
Techwolf78 <mrajaypawar207@gmail.com>
2025-02-11 11:26:40 +0530

    hello

--- src/components/Sales/Components/AffiliationSelect.jsx
+++ src/components/Sales/Components/AffiliationSelect.jsx
@@ -1,61 +1,76 @@
 import PropTypes from 'prop-types';
 
 const AffiliationSelect = ({ affiliation, otherAffiliation, handleChange }) => {
-    return (
-      <div>
-        <label className="block text-sm font-medium">Affiliation</label>
-        <select
-          name="affiliation"
-          value={affiliation}
-          onChange={handleChange}
-          className="p-2 w-full border-2 rounded-lg border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none"
-        >
-          <option value="">Select Affiliation</option>
-        <option value="Autonomous">Autonomous</option>
-        <option value="SPPU">Savitribai Phule Pune University (SPPU)</option>
-        <option value="Mumbai">University of Mumbai</option>
-        <option value="Shivaji">Shivaji University (Kolhapur)</option>
-        <option value="Ambedkar">Dr. Babasaheb Ambedkar Marathwada University (Aurangabad)</option>
-        <option value="Nagpur">Rashtrasant Tukadoji Maharaj Nagpur University</option>
-        <option value="Amravati">Sant Gadge Baba Amravati University</option>
-        <option value="North Maharashtra">North Maharashtra University (Jalgaon)</option>
-        <option value="Solapur">Solapur University</option>
-        <option value="SRTMU">Swami Ramanand Teerth Marathwada University (Nanded)</option>
-        <option value="YCMOU">Yashwantrao Chavan Maharashtra Open University (Nashik)</option>
-        <option value="DBATU">Dr. Babasaheb Ambedkar Technological University (Lonere)</option>
-        <option value="MUHS">Maharashtra University of Health Sciences (Nashik)</option>
-        <option value="MAFSU">Maharashtra Animal and Fishery Sciences University (Nagpur)</option>
-        <option value="MPKV">Mahatma Phule Krishi Vidyapeeth (Rahuri)</option>
-        <option value="PDKV">Dr. Panjabrao Deshmukh Krishi Vidyapeeth (Akola)</option>
-        <option value="VNMKV">Vasantrao Naik Marathwada Krishi Vidyapeeth (Parbhani)</option>
-        <option value="DBSKKV">Dr. Balasaheb Sawant Konkan Krishi Vidyapeeth (Dapoli)</option>
-        <option value="MNLU_Mumbai">Maharashtra National Law University (Mumbai)</option>
-        <option value="MNLU_Nagpur">Maharashtra National Law University (Nagpur)</option>
-        <option value="MNLU_Aurangabad">Maharashtra National Law University (Aurangabad)</option>
-        <option value="HBSU">Dr. Homi Bhabha State University (Mumbai)</option>
-        <option value="HSNC">HSNC University (Mumbai)</option>
-        <option value="Deccan">Deccan College Postgraduate and Research Institute (Pune)</option>
-        <option value="GIPE">Gokhale Institute of Politics and Economics (Pune)</option>
-        <option value="TMV">Tilak Maharashtra Vidyapeeth (Pune)</option>
-        <option value="TISS">Tata Institute of Social Sciences (Mumbai)</option>
-        <option value="SIU">Symbiosis International University (Pune)</option>
-        <option value="Bharati">Bharati Vidyapeeth (Pune)</option>
-        <option value="DYPatil">D.Y. Patil University (Navi Mumbai)</option>
-        <option value="MIT_ADT">MIT Art, Design and Technology University (Pune)</option>
-        <option value="ADYPU">Ajeenkya D Y Patil University (Pune)</option>
-        <option value="Flame">Flame University (Pune)</option>
-        <option value="Amity">Amity University (Mumbai)</option>
-        <option value="Sandip">Sandip University (Nashik)</option>
-        <option value="CSMU">Chhatrapati Shivaji Maharaj University (Navi Mumbai)</option>
-        <option value="MGM">MGM University (Aurangabad)</option>
-        <option value="Vishwakarma">Vishwakarma University (Pune)</option>
-        <option value="Spicer">Spicer Adventist University (Pune)</option>
-        <option value="SSPU">Symbiosis Skills and Professional University (Pune)</option>
-        <option value="MIT_WPU">MIT World Peace University (Pune)</option>
-        <option value="ICT">Institute of Chemical Technology (Mumbai)</option>
-        <option value="VJTI">Veermata Jijabai Technological Institute (Mumbai)</option>
-        <option value="COEP">College of Engineering Pune (Pune)</option>
-        <option value="Other">Other</option>
+  const affiliationOptions = [
+    "Autonomous",
+    "Savitribai Phule Pune University (SPPU)",
+    "University of Mumbai",
+    "Shivaji University (Kolhapur)",
+    "Dr. Babasaheb Ambedkar Marathwada University (Aurangabad)",
+    "Rashtrasant Tukadoji Maharaj Nagpur University",
+    "Sant Gadge Baba Amravati University",
+    "North Maharashtra University (Jalgaon)",
+    "Solapur University",
+    "Swami Ramanand Teerth Marathwada University (Nanded)",
+    "Yashwantrao Chavan Maharashtra Open University (Nashik)",
+    "Dr. Babasaheb Ambedkar Technological University (Lonere)",
+    "Maharashtra University of Health Sciences (Nashik)",
+    "Maharashtra Animal and Fishery Sciences University (Nagpur)",
+    "Mahatma Phule Krishi Vidyapeeth (Rahuri)",
+    "Dr. Panjabrao Deshmukh Krishi Vidyapeeth (Akola)",
+    "Vasantrao Naik Marathwada Krishi Vidyapeeth (Parbhani)",
+    "Dr. Balasaheb Sawant Konkan Krishi Vidyapeeth (Dapoli)",
+    "Maharashtra National Law University (Mumbai)",
+    "Maharashtra National Law University (Nagpur)",
+    "Maharashtra National Law University (Aurangabad)",
+    "Dr. Homi Bhabha State University (Mumbai)",
+    "HSNC University (Mumbai)",
+    "Deccan College Postgraduate and Research Institute (Pune)",
+    "Gokhale Institute of Politics and Economics (Pune)",
+    "Tilak Maharashtra Vidyapeeth (Pune)",
+    "Tata Institute of Social Sciences (Mumbai)",
+    "Symbiosis International University (Pune)",
+    "Bharati Vidyapeeth (Pune)",
+    "D.Y. Patil University (Navi Mumbai)",
+    "MIT Art, Design and Technology University (Pune)",
+    "Ajeenkya D Y Patil University (Pune)",
+    "Flame University (Pune)",
+    "Amity University (Mumbai)",
+    "Sandip University (Nashik)",
+    "Chhatrapati Shivaji Maharaj University (Navi Mumbai)",
+    "MGM University (Aurangabad)",
+    "Vishwakarma University (Pune)",
+    "Spicer Adventist University (Pune)",
+    "Symbiosis Skills and Professional University (Pune)",
+    "MIT World Peace University (Pune)",
+    "Institute of Chemical Technology (Mumbai)",
+    "Veermata Jijabai Technological Institute (Mumbai)",
+    "College of Engineering Pune (Pune)",
+    "Other"
+  ];
+
+  // Sort the options alphabetically
+  const sortedAffiliationOptions = affiliationOptions.sort();
+
+  // Move "Other" to the end of the list
+  const finalAffiliationOptions = sortedAffiliationOptions.filter(option => option !== "Other");
+  finalAffiliationOptions.push("Other");
+
+  return (
+    <div>
+      <label className="block text-sm font-medium">Affiliation</label>
+      <select
+        name="affiliation"
+        value={affiliation}
+        onChange={handleChange}
+        className="p-2 w-full border-2 rounded-lg border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none"
+      >
+        <option value="">Select Affiliation</option>
+        {finalAffiliationOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
 
       {/* Show this input only if "Other" is selected */}
